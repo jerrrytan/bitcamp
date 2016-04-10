62dd7ed3dc99ccd92d5c7255132833636013874f
Jerry <jtan@localhost.localdomain>
2016-04-09 21:53:41 -0400

    added some url support

--- bitcamp/bitcampapp/meal_plan_gen.py
+++ bitcamp/bitcampapp/meal_plan_gen.py
@@ -17,27 +17,29 @@ def get_meal_plan(preferences, budget):
     while len(recipes_to_ingredients.keys()) < NUM_MEALS:
         category = random.randrange(6)
         if category == 0:
-            recipe_to_ingredients = getIngredients(preferences[0])
+            recipe_to_ingredients, recipe_to_url = getIngredients(preferences[0])
             recipes_to_ingredients.update(recipe_to_ingredients)
         elif category == 1 or category == 2:
-            recipe_to_ingredients = getIngredients(preferences[1])
+            recipe_to_ingredients, recipes_to_url = getIngredients(preferences[1])
             recipes_to_ingredients.update(recipe_to_ingredients)
         else:
-            recipe_to_ingredients = getIngredients(preferences[2])
+            recipe_to_ingredients, recipes_to_url = getIngredients(preferences[2])
             recipes_to_ingredients.update(recipe_to_ingredients)
 
     recipes = []
     costs = []
+    recipe_urls = []
     total_cost = 0
     for (recipe, cost) in (load_recipes(recipes_to_ingredients)).keys():
         recipes.append(recipe)
         costs.append(cost)
+        recipe_urls.append(recipes_to_url[recipe])
         total_cost += cost
 
     if total_cost/6 > budget:
         raise Exception("Insufficient Budget!")
 
     else:
-        for (meal, cost) in zip(recipes, costs):
-            m = Meal(meal_name=meal, meal_cost=math.floor(cost))
+        for (meal, cost, recipe_url) in zip(recipes, costs, recipe_urls):
+            m = Meal(meal_name=meal, meal_cost=math.floor(cost), meal_url=recipe_url)
             m.save()
--- bitcamp/bitcampapp/models.py
+++ bitcamp/bitcampapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Meal(models.Model):
     meal_name = models.CharField(max_length=200)
     meal_cost = models.IntegerField(default=10)
+    meal_url = models.CharField(max_length=200)
 
     def __str__(self):
         return self.meal_name
